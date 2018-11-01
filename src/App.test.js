be6e7e78e94b4cb5b3d77e450a4b90c2f2b89910
Gordon Freeman <godon019@gmail.com>
2018-11-01 12:23:44 +0900

    Shallow tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import { shallow } from 'enzyme';
+import { default as React, } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+//testing only App and not going deep
+it('shallow test', () => {
+  shallow(<App />);
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
